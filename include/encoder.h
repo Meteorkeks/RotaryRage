9e1dc15aa6ee2e0a7e13ef3f866d8a4e8d81871f
max <max92sauer@gmx.de>
2021-08-15 11:35:13 +0200

    Add header files of refactored encoder/bouncer

---
+++ include/encoder.h
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include "encoder.h"
+
+class Encoder {
+  public:
+    // pin numbers
+    int enA_pin, enB_pin, bttn_pin;
+    bool stateA = LOW;
+    bool stateB = LOW;    
+    
+    // continuous rotary value
+    int value = 0;
+    int value_last = 0;
+    
+    // push button state
+    bool button = false;
+    bool button_last = false;
+
+    // debounce wrapper for pins
+    Bounce * enA;
+    Bounce * enB;
+    Bounce * bttn;
+    
+    /**
+     * @brief Constructs a new rotary encoder object
+     * 
+     * @param encoderA pin encoder A
+     * @param encoderB pin encoder B
+     * @param taster pin push button
+     */
+    Encoder(uint8_t encoderA, uint8_t encoderB, uint8_t taster);
+  
+    void update();
+
+    int getValueDiff();
+
+    bool getButtonDiff();
+};
