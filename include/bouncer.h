e20bd7f7a0c2b156a9544408ea00cdd0df8cb365
Max <max92sauer@gmx.de>
2021-08-15 14:34:26 +0200

    Upate bouncer
    Co-authored-by: Sandro Braun <theRealSuperMario@users.noreply.github.com>

--- include/bouncer.h
+++ include/bouncer.h
@@ -12,17 +12,17 @@ class Bounce {
         uint8_t pin_number;
         uint16_t timeout_ms; 
 
+        Bounce(uint8_t pin_number, uint16_t timeout);
+
         // return true if last update() leads to xxxEdge
         bool fallingEdge();
         bool risingEdge();
-
-
-        Bounce(uint8_t pin_number, uint16_t timeout);
-
+        bool change();
         bool update() ;
 
     private:
         bool last_state;
+        bool current_state;
         u_long state_time;
         bool is_debouncing;
 };
--- src/bouncer.cpp
+++ src/bouncer.cpp
@@ -2,10 +2,22 @@
 
 Bounce::Bounce(uint8_t pin_number, uint16_t timeout)
 {
-    pin_number = pin_number;
-    timeout = timeout;
+    this->pin_number = pin_number;
+    timeout_ms = timeout;
+    is_debouncing = false;
+    last_state = false;
+    current_state = false;
 }
 
+
+/**
+ * @brief 
+ * 
+ * See slides for more https://docs.google.com/presentation/d/1O3VIUvFkATJrm6YaQaJuq9ZdTl-Y60neox94lireewk/edit#slide=id.ge8b705876d_0_0
+ * 
+ * @return true 
+ * @return false 
+ */
 bool Bounce::update() {
     // get current state
     int current_state = digitalRead(pin_number);
@@ -14,21 +26,53 @@ bool Bounce::update() {
         u_long new_time_stamp = millis();
         u_long time_elapsed_since_trigger_event = new_time_stamp - state_time;
         if (time_elapsed_since_trigger_event > timeout_ms) {
-            last_state = current_state;
+            // propagate change
+            this->current_state = current_state;
+            is_debouncing = false;
             return true;
         }
+        return false;
     }
     else {
-        if (current_state ^ last_state) {
+        bool needs_debouncing = current_state ^ last_state;
+        if (needs_debouncing) {
             // start debouncing
             state_time = millis();
             is_debouncing = true;
+            return false;
         }
         else {
+            // pass through state change
+            this->last_state = this->current_state;
+            this->current_state = current_state;
             return false;
         }
     }
 }
 
-bool Bounce::fallingEdge() {}
-bool Bounce::risingEdge() {}
+bool Bounce::fallingEdge() 
+{
+    return this->last_state && !this->current_state;
+}
+bool Bounce::risingEdge()
+{
+    return !this->last_state && this->current_state;
+}
+
+bool Bounce::change()
+{
+    //https://stackoverflow.com/a/1596681/10045897
+    return this->last_state != this->current_state;
+}
+
+
+/*
+Example usage
+
+if (bounce->update) {
+    if (bounce->risingEdge()) {
+        return True
+    }
+}
+
+*/
